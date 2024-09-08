7d51179dbc5afae4bfc856bf2e993b2c067dc8dc
Kevin <moralesperezkevin7@gmail.com>
2024-09-08 14:58:30 -0600

    Second commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const publicacionesRoutes = require('./routes/publicaciones');
+
+const app = express();
+const port = 3000;
+
+// Middleware para analizar los cuerpos de las solicitudes
+app.use(bodyParser.json());
+
+//usar las rutas de los items
+app.use('/publicaciones', publicacionesRoutes);
+
+//iniciar el servidor
+app.listen(port, () =>{
+    console.log(`Servidor Express en ejecución en http://localhost:${port}`);
+});
+
+
+

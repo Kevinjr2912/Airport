720a70a0a55dc192e78c2cc0ec043b94bcccc64d
Kevin <moralesperezkevin7@gmail.com>
2024-09-08 21:46:59 -0600

    Funcionalidad de consultar el estado de la reserva

--- src/routes/reservations.js
+++ src/routes/reservations.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const reservationsControllers = require('../controllers/reservations')
 
 router.put('/cancel/:id',)
+router.get('/getStatusReservation',reservationsControllers.knowReservationStatus);
 
 module.exports = router;
